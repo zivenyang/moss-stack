e1bb0e84d2f7ad364b79a89c2e18710924838c76
杨子文 <yangziwen1994@gmail.com>
2025-09-26 11:05:24 +0800

    feat: 新增前端项目基础结构，包含配置文件、路由设置、组件和样式，集成TanStack和Tailwind CSS

---
+++ frontend/src/routes/__root.tsx
@@ -0,0 +1,25 @@
+import { Outlet, createRootRoute } from '@tanstack/react-router'
+import { TanStackRouterDevtoolsPanel } from '@tanstack/react-router-devtools'
+import { TanstackDevtools } from '@tanstack/react-devtools'
+
+import Header from '../components/Header'
+
+export const Route = createRootRoute({
+  component: () => (
+    <>
+      <Header />
+      <Outlet />
+      <TanstackDevtools
+        config={{
+          position: 'bottom-left',
+        }}
+        plugins={[
+          {
+            name: 'Tanstack Router',
+            render: <TanStackRouterDevtoolsPanel />,
+          },
+        ]}
+      />
+    </>
+  ),
+})
